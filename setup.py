93652a53c52756f476d7a2d09b9af259b44553a2
Cody Clerke <codyclerke@live.ca>
2015-03-25 16:33:15 -0700

    Add setup files for PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils import setup
+setup(
+    name = 'bananatag-api',
+    packages = ['bananatag-api'],
+    version = '1.0',
+    description = 'The Bananatag API Python Library is used in conjunction with Bananatag\'s REST API. The Bananatag REST API allows users access to all data associated with their account and sub-accounts.',
+    author = 'Eric Wein',
+    author_email = 'eric@bananatag.com',
+    url = 'https://github.com/bananatag/bananatag-api-python',
+    download_url = 'https://github.com/bananatag/bananatag-api-python/tarball/1.0',
+    keywords = [],
+    classifiers = []
+)
