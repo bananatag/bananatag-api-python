f4aa8babc6ca1f216d922502e7dc52c2cfe79270
Cody Clerke <codyclerke@live.ca>
2015-04-06 14:02:43 -0700

    Add support for sending basic HTML emails with attachments

--- btapi/btapi.py
+++ btapi/btapi.py
@@ -14,6 +14,11 @@ import hmac
 import base64
 from hashlib import sha1
 
+import mimetypes
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 
 class BTagAPI:
     def __init__(self, auth_id, access_key):
@@ -29,6 +34,8 @@ class BTagAPI:
         self.auth_id = auth_id
         self.access_key = access_key
         self.base_url = 'https://api.bananatag.com/'
+
+        self.post_endpoints = ['tags/send']
 
         self.last_endpoint = None
         self.last_params = {}
@@ -50,9 +57,13 @@ class BTagAPI:
         else:
             self.validate_data(params)
             self.save_current_request(endpoint, params)
+            url = urllib.quote(self.base_url + endpoint, ':/-')
             query_string = self.build_data_string(params)
-            url = urllib.quote(self.base_url + endpoint, ':/-') + '?' + query_string
-            result = self.make_request(url, query_string)
+            post = endpoint in self.post_endpoints
+            if not post:
+                url += '?' + query_string
+
+            result = self.make_request(url, query_string, post)
 
         try:
             self.save_response(result.get('paging'))
@@ -62,16 +73,20 @@ class BTagAPI:
 
         return result
 
-    def make_request(self, url, params):
+    def make_request(self, url, params, post=False):
         """
         Makes request and returns JSON data
 
         :param url: URL endpoint of request
-        :param params: Dictionary of request parameters
+        :param params: URL encoded string of request parameters
+        :param post: Whether the request method should be POST (default: False)
         :return: JSON object with request response data
         """
         request_headers = {'Authorization': base64.b64encode(self.auth_id + ':' + self.generate_signature(params))}
-        request = urllib2.Request(url, headers=request_headers)
+        if post:
+            request = urllib2.Request(url, params, headers=request_headers)
+        else:
+            request = urllib2.Request(url, headers=request_headers)
 
         try:
             response = urllib2.urlopen(request)
@@ -149,3 +164,64 @@ class BTagAPI:
         :param data: Paging data returned from current request
         """
         self.next_url = data.get('nextURL')
+
+    @staticmethod
+    def build_message(params):
+        """
+        Builds an HTML email message with optional attachments
+        :param params: Dictionary of parameters to build email with
+        :return: Base64 encoded raw message body
+        """
+        if params.get('from') is None:
+            raise Exception('You must specify the From header.')
+        if params.get('to') is None:
+            raise Exception('You must specify the To header.')
+        elif not isinstance(params.get('to'), list):
+            raise Exception('To header must be specified as a list.')
+        if params.get('html') is None:
+            raise Exception('You must include the html parameter.')
+
+        attachments = params.get('attachments')
+        if attachments is not None:
+            if not isinstance(attachments, list):
+                raise Exception('Attachments must be a list.')
+
+        msg = MIMEMultipart()
+        msg['Subject'] = params.get('subject')
+        msg['From'] = params.get('from')
+        msg['To'] = ','.join(params.get('to'))
+        msg.attach(MIMEText(params.get('html'), 'html', 'utf-8'))
+
+        headers = params.get('headers')
+        if headers:
+            for header, value in headers:
+                msg.add_header(header, value)
+
+        if attachments is not None:
+            for item in attachments:
+                fname = item.get('filename')
+                if fname is None:
+                    raise Exception('Attachment filename must be specified.')
+
+                if item.get('content-type'):
+                    content_type = item.get('content-type')
+                else:
+                    content_type, encoding = mimetypes.guess_type(fname)
+                    if content_type is None or encoding is not None:
+                        content_type = 'application/octet-stream'
+                main, sub = content_type.split('/', 1)
+                attachment = MIMEBase(main, sub)
+
+                attachment.set_payload(item.get('contents'))
+
+                if item.get('content-disposition') == 'inline' and 'content-id' in item:
+                    attachment.add_header('Content-ID', item.get('content-id'))
+
+                attachment.add_header('Content-Disposition', item.get('content-disposition'), filename=fname)
+
+                if item.get('content-transfer-encoding') is not None:
+                    attachment.add_header('Content-Transfer-Encoding', item.get('content-transfer-encoding'))
+
+                msg.attach(attachment)
+
+        return base64.b64encode(msg.as_string())
